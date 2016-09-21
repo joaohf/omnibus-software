42a9fd0fad9f7803d3e2c457e89277a3228e30a3
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2016-09-21 17:07:24 -0300

    Add rabbitmq 3.6.5

--- config/software/rabbitmq.rb
+++ config/software/rabbitmq.rb
@@ -23,12 +23,29 @@ skip_transitive_dependency_licensing true
 
 dependency "erlang"
 
-version("3.6.0") { source md5: "61a3822f3af0aaa30da7230dccb17067" }
-version("3.3.4") { source md5: "61a3822f3af0aaa30da7230dccb17067" }
-version("2.8.7") { source md5: "35e8d78f8d7ae4372db23fe50db82c64" }
-version("2.7.1") { source md5: "34a5f9fb6f22e6681092443fcc80324f" }
+version("3.6.5") {
+  source md5: "9a603ca54bdd3ee70cc2a9b52a0e58fd",
+         url: "https://www.rabbitmq.com/releases/rabbitmq-server/v#{version}/rabbitmq-server-generic-unix-#{version}.tar.xz"
+}
+version("3.6.0") { 
+  source md5: "61a3822f3af0aaa30da7230dccb17067",
+         url: "https://www.rabbitmq.com/releases/rabbitmq-server/v#{version}/rabbitmq-server-generic-unix-#{version}.tar.xz"
+}
+version("3.3.4") {
+  source md5: "61a3822f3af0aaa30da7230dccb17067",
+         url: "https://www.rabbitmq.com/releases/rabbitmq-server/v#{version}/rabbitmq-server-generic-unix-#{version}.tar.gz"
 
-source url: "https://www.rabbitmq.com/releases/rabbitmq-server/v#{version}/rabbitmq-server-generic-unix-#{version}.tar.gz"
+}
+version("2.8.7") {
+  source md5: "35e8d78f8d7ae4372db23fe50db82c64",
+         url: "https://www.rabbitmq.com/releases/rabbitmq-server/v#{version}/rabbitmq-server-generic-unix-#{version}.tar.gz"
+
+}
+version("2.7.1") {
+  source md5: "34a5f9fb6f22e6681092443fcc80324f",
+         url: "https://www.rabbitmq.com/releases/rabbitmq-server/v#{version}/rabbitmq-server-generic-unix-#{version}.tar.gz"
+
+}
 
 relative_path "rabbitmq_server-#{version}"
 
